2ef7733089ac85f7339af98c3e5b476f3c9b69a2
Suraj Dutta <surajdutta@Surajs-MacBook-Pro.local>
2021-04-01 08:55:08 +0800

    added selenium and took care of the image url of relative path

--- routers/metadata.py
+++ routers/metadata.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter, HTTPException
+from selenium import webdriver
 from bs4 import BeautifulSoup
-import requests, json
+from urllib.parse import urlparse
+import requests, json, re
 
 router = APIRouter()
 
@@ -8,12 +10,17 @@ router = APIRouter()
 def getMetadata(url: str = ''):
 
     try:
-        response = requests.get(url)
-        soup = BeautifulSoup(response.text, 'html.parser')
+        driver = webdriver.Firefox()
+        driver.get(url)
+        soup = BeautifulSoup(driver.page_source, 'html.parser')
 
         title = get_title(soup)
         description = get_description(soup)
         image = get_image(soup)
+        if image:
+            image = validate_image_url(image, url)
+
+        driver.close()
 
         return {
             "url": url,
@@ -26,31 +33,39 @@ def getMetadata(url: str = ''):
         raise HTTPException(status_code=404, detail='Connection error')
 
 
-
 def get_title(soup):
 
     title = soup.find('title')
-    if title: return [title.string]
-    
+    if title:
+        return [title.string]
+
     title = soup.find("meta",  property="og:title")
-    if title: return [title['content']]
-    title = soup.find("meta", attrs={'name':'og:title'})
-    if title: return [title['content']]
+    if title:
+        return [title['content']]
+    title = soup.find("meta", attrs={'name': 'og:title'})
+    if title:
+        return [title['content']]
 
     title = soup.find("meta",  property="twitter:title")
-    if title: return [title['content']]
-    title = soup.find("meta", attrs={'name':'twitter:title'})
-    if title: return [title['content']]
+    if title:
+        return [title['content']]
+    title = soup.find("meta", attrs={'name': 'twitter:title'})
+    if title:
+        return [title['content']]
 
     title = soup.find("meta",  property="title")
-    if title: return [title['content']]
-    title = soup.find("meta", attrs={'name':'title'})
-    if title: return [title['content']]
+    if title:
+        return [title['content']]
+    title = soup.find("meta", attrs={'name': 'title'})
+    if title:
+        return [title['content']]
 
     title = soup.find("meta",  property="Title")
-    if title: return [title['content']]
-    title = soup.find("meta", attrs={'name':'Title'})
-    if title: return [title['content']]
+    if title:
+        return [title['content']]
+    title = soup.find("meta", attrs={'name': 'Title'})
+    if title:
+        return [title['content']]
 
     return []
 
@@ -58,24 +73,32 @@ def get_title(soup):
 def get_description(soup):
 
     description = soup.find("meta",  property="og:description")
-    if description: return [description['content']]
-    description = soup.find("meta", attrs={'name':'og:description'})
-    if description: return [description['content']]
+    if description:
+        return [description['content']]
+    description = soup.find("meta", attrs={'name': 'og:description'})
+    if description:
+        return [description['content']]
 
     description = soup.find("meta",  property="twitter:description")
-    if description: return [description['content']]
-    description = soup.find("meta", attrs={'name':'twitter:description'})
-    if description: return [description['content']]
+    if description:
+        return [description['content']]
+    description = soup.find("meta", attrs={'name': 'twitter:description'})
+    if description:
+        return [description['content']]
 
     description = soup.find("meta",  property="description")
-    if description: return [description['content']]
-    description = soup.find("meta",  attrs={'name':'description'})
-    if description: return [description['content']]
+    if description:
+        return [description['content']]
+    description = soup.find("meta",  attrs={'name': 'description'})
+    if description:
+        return [description['content']]
 
     description = soup.find("meta",  property="Description")
-    if description: return [description['content']]
-    description = soup.find("meta", attrs={'name':'Description'})
-    if description: return [description['content']]
+    if description:
+        return [description['content']]
+    description = soup.find("meta", attrs={'name': 'Description'})
+    if description:
+        return [description['content']]
 
     return []
 
@@ -83,29 +106,48 @@ def get_description(soup):
 def get_image(soup):
 
     image = soup.find("meta",  property="og:image")
-    
-    if image: return [image['content']]
-    image = soup.find("meta", attrs={'name':'og:image'})
-    if image: return [image['content']]
+
+    if image:
+        return [image['content']]
+    image = soup.find("meta", attrs={'name': 'og:image'})
+    if image:
+        return [image['content']]
 
     image = soup.find("meta",  property="twitter:image")
-    if image: return [image['content']]
-    image = soup.find("meta", attrs={'name':'twitter:image'})
-    if image: return [image['content']]
+    if image:
+        return [image['content']]
+    image = soup.find("meta", attrs={'name': 'twitter:image'})
+    if image:
+        return [image['content']]
 
     image = soup.find("meta",  property="msapplication-TileImage")
-    if image: return [image['content']]
-    image = soup.find("meta", attrs={'name':'msapplication-TileImage'})
-    if image: return [image['content']]
+    if image:
+        return [image['content']]
+    image = soup.find("meta", attrs={'name': 'msapplication-TileImage'})
+    if image:
+        return [image['content']]
 
     image = soup.find("meta",  property="image")
-    if image: return [image['content']]
-    image = soup.find("meta", attrs={'name':'image'})
-    if image: return [image['content']]
+    if image:
+        return [image['content']]
+    image = soup.find("meta", attrs={'name': 'image'})
+    if image:
+        return [image['content']]
 
     image = soup.find("meta",  property="Image")
-    if image: return [image['content']]
-    description = soup.find("meta", attrs={'name':'Image'})
-    if image: return [image['content']]
+    if image:
+        return [image['content']]
+    description = soup.find("meta", attrs={'name': 'Image'})
+    if image:
+        return [image['content']]
 
     return []
+
+
+def validate_image_url(image, url):
+    if not re.match('(?:http|ftp|https)://', image[0]):
+        parsed_uri = urlparse(url)
+        result = '{uri.scheme}://{uri.netloc}'.format(uri=parsed_uri)
+        return [result + image[0]]
+    else:
+        return image
